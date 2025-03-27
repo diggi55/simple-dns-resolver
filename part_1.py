941fad49f851ebb41be5f609c668de0b7aef3be4
diggi55 <schaeffer-tobias@gmx.de>
2025-03-27 14:33:23 +0100

    Rename part_1 according to RFC 1035

--- part_1.py
+++ part_1.py
@@ -4,31 +4,36 @@ import struct
 
 
 @dataclass
-class DNSHeader:
+class Header:
     id: int
     flags: int
-    num_questions: int = 0
-    num_answers: int = 0
-    num_authorities: int = 0
-    num_additionals: int = 0
-
-
-@dataclass
-class DNSQuestion:
-    name: bytes
-    type_: int
-    class_: int
-
+    questions_count: int = 0
+    answers_count: int = 0
+    namer_servers_count: int = 0
+    additional_records_count: int = 0
 
 def header_to_bytes(header):
     fields = dataclasses.astuple(header)
     return struct.pack('!HHHHHH', *fields)
 
 
+@dataclass
+class Question:
+    qname: bytes
+    qtype: int
+    qclass: int
+
+
 def question_to_bytes(question):
-    return question.name + struct.pack('!HH', question.type_, question.class_)
+    return question.qname + struct.pack('!HH', question.qtype, question.qclass)
 
 
+# TODO
+# Wo ist das festgelegt? 
+#   1.) Erst Länge, dann Teil der Domain 
+#   2.) 0er Byte zum Schluss?
+# Anschauen: RFC 1035
+# TODO: Warum ist von dns name und nicht von domain name die Rede?
 def encode_dns_name(domain_name):
     encoded = b''
     for part in domain_name.encode('ascii').split(b'.'):
@@ -37,6 +42,7 @@ def encode_dns_name(domain_name):
 
 
 import random
+random.seed(1)
 TYPE_A = 1
 CLASS_IN = 1
 
@@ -45,9 +51,23 @@ def build_query(domain_name, record_type):
     name = encode_dns_name(domain_name)
     id = random.randint(0, 65535)
     RECURSION_DESIRED = 1 << 8
-    header = DNSHeader(id=id, num_questions=1, flags=RECURSION_DESIRED)
-    question = DNSQuestion(name=name, type_=record_type, class_=CLASS_IN)
+    header = Header(id=id, questions_count=1, flags=RECURSION_DESIRED)
+    question = Question(qname=name, qtype=record_type, qclass=CLASS_IN)
     return header_to_bytes(header) + question_to_bytes(question)
 
 
 import socket
+
+query = build_query("example.com", TYPE_A)
+# hex query for example.com:
+# 44cb01000001000000000000076578616d706c6503636f6d0000010001
+# hex query for google.com:
+# 44cb0100000100000000000006676f6f676c6503636f6d0000010001
+if query.hex() == "44cb01000001000000000000076578616d706c6503636f6d0000010001":
+    print("QUERY PASST!")
+else:
+    print("QUERY FALSCH!!!")
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.sendto(query, ("8.8.8.8", 53))
+response, _ = sock.recvfrom(1024)
